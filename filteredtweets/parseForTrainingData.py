9c4697ba62b0924ae9f0bb7df937d6531bb56f22
franklinharvey <franklinadamsharvey@gmail.com>
2018-04-28 11:17:07 -0600

    yay we have so many tweets now

--- filteredtweets/parseForTrainingData.py
+++ filteredtweets/parseForTrainingData.py
@@ -7,6 +7,7 @@ def main():
     results = []
     with open("harvey2.csv", encoding='mac_roman') as csvfile:
         reader = csv.reader(csvfile) # change contents to floats
+        print("Parsing")
         for row in reader: # each row is a list
             results.append(row)
 
@@ -15,16 +16,22 @@ def main():
     # print(newResults[1]["index"])
     newResults = []
     resultsCount = 0
+    exceptions = 0
+    print("Copying Data")
     for row in results:
-        newResults.append({"index": row[0], "id": row[1], "date":  row[2], "text": row[3], "url": row[4], "location": row[5]})
-        resultsCount += 1
+        try:
+            newResults.append({"index": row[0], "id": row[1], "date":  row[2], "text": row[3], "url": row[4], "location": row[5]})
+            resultsCount += 1
+        except:
+            exceptions += 1
     del results
 
     hurricaneTweets = []
     weinsteinTweets = []
     hurricaneCount = 0
     weinsteinCount = 0
-    for row in newResults:
+    print("Searching for key words")
+    for n,row in enumerate(newResults):
         for word in hurricaneKeyWords:
             if word in row["text"].lower():
                 hurricaneTweets.append(row)
@@ -35,14 +42,20 @@ def main():
                 weinsteinTweets.append(row)
                 weinsteinCount += 1
                 break
+        if n>500000:
+            break
+    del newResults
 
     with open('hurricane.json', 'w') as outfile:
+        print("Writing hurricane.json")
         json.dump(hurricaneTweets, outfile, indent=4, sort_keys=True,)
 
     with open('weinstein.json', 'w') as outfile:
+        print("Writing weinstein.json")
         json.dump(weinsteinTweets, outfile, indent=4, sort_keys=True,)
 
     print("Total Results: %s" % (resultsCount))
+    print("Excepted: %s" % (exceptions))
     print("Hurricane Results: %s" % (hurricaneCount))
     print("Weinstein Results: %s" % (weinsteinCount))
 
